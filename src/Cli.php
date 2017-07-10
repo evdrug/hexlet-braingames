bea556aac93febd4e1c3562b539e5f09ef0cc929
Eugene Sinitsyn <phollyolly@gmail.com>
2017-07-10 23:19:20 +0300

    fix code style

--- src/Cli.php
+++ src/Cli.php
@@ -4,7 +4,8 @@ namespace Braingames\Cli;
 
 use function \cli\line;
 
-function run() {
+function run()
+{
     line('Welcome to the Brain Games!');
     $name = \cli\prompt('May I have your name?');
     line("Hello, %s", $name);
